c4f4f1229fc98a710f6142cc75697cedebccd954
gerbsec <albabawaseem@gmail.com>
2022-01-26 11:39:03 -0500

    some progress

--- lol.sh
+++ lol.sh
@@ -1,2 +1,3 @@
-test=$(curl https://gtfobins.github.io/\#+suid 2>/dev/null | grep "class=\"bin-name\"" | awk -F '>' '{print $3}' | awk -F '<' '{print $1}' | tr '\n' ' ')
+# test=$(curl https://gtfobins.github.io/\#+suid 2>/dev/null | grep "class=\"bin-name\"" | awk -F '>' '{print $3}' | awk -F '<' '{print $1}' | tr '\n' ' ')
+test=($(curl https://gtfobins.github.io/\#+suid 2>/dev/null | grep "class=\"bin-name\"" | awk -F '>' '{print $3}' | awk -F '<' '{print $1}' | tr '\n' ' ' | xargs printf "\"%s\" "))
 echo "$test";
