0948ded24c7492384c68541582464f35cd5b9977
gerbsec <67713732+gerbsec@users.noreply.github.com>
2022-02-01 20:28:33 +0000

    Cleaned up the code

--- v3.sh
+++ v3.sh
@@ -1,2 +1,13 @@
-command eval  "suidGTFO=($(cat test.txt | tr '\n' ' '))"
-echo "${#suidGTFO[@]}"
+eval "suidGTFO=($(cat payload.txt | tr '\n' ' '))"
+eval "suidOM=($(find / -perm -4000 -type f 2>/dev/null | sed 's:.*/\(.*\):"\1":' | tr '\n' ' '))"
+exploitables=()
+for eSuid in ${suidGTFO[@]};do
+    for suid in ${suidOM[@]};do
+        if [[ "$eSuid" == *"$suid"* ]];then
+            exploitables+=("$suid")
+            fi
+        done;
+    done;
+for i in ${exploitables[@]};do
+    echo '{"suid": '\"$i\"'}'
+done;
